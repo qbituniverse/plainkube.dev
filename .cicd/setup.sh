a2db400ae37c498884bb1635216d34c40fadeab2
qbituniverse <48298870+qbituniverse@users.noreply.github.com>
2023-10-26 15:28:35 +0200

    Update setup.sh

--- .cicd/setup.sh
+++ .cicd/setup.sh
@@ -2,10 +2,10 @@
 # Docker Compose [Development]
 #############################################################################
 # start up
-docker-compose -f .cicd/docker-compose.Development.yaml up
+docker compose -f .cicd/docker-compose.Development.yaml up
 
 # browse
 start http://localhost:4000
 
 # take down
-docker-compose -f .cicd/docker-compose.Development.yaml down
+docker compose -f .cicd/docker-compose.Development.yaml down
